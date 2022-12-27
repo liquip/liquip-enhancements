a04b8ea7dc418bbd039cef46729488b01bfe3158
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-27 12:45:57 +0100

    Add basic Recipe Book implementation

--- src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
+++ src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
@@ -3,20 +3,41 @@ package io.github.liquip.enhancements
 import com.github.sqyyy.cougar.Cougar
 import com.github.sqyyy.cougar.Slot
 import com.github.sqyyy.cougar.Ui
+import com.github.sqyyy.cougar.impl.panel.ClickPanel
 import com.github.sqyyy.cougar.impl.panel.SingleSlotClickPanel
 import com.github.sqyyy.cougar.impl.paperUi
 import io.github.liquip.api.LiquipProvider
+import io.github.liquip.api.item.Item
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
+import java.util.stream.Collectors
+import java.util.stream.StreamSupport
+import kotlin.math.max
+import kotlin.math.min
 
 class LiquipEnhancementsPlugin : JavaPlugin() {
+    private val itemsPerPage = 7 * 4
+    private val recipeBookPages = Object2IntOpenHashMap<UUID>()
+    private var pageCount = 3
+    private var catalogue = listOf<Pair<Item, ItemStack>>()
+
     override fun onEnable() {
         Cougar.initializeSystem(this)
         val api = LiquipProvider.get()
+        catalogue = StreamSupport.stream(api.itemRegistry.spliterator(), false)
+            .map { it to it.newItemStack() }
+            .collect(Collectors.toUnmodifiableList())
+        val size = catalogue.size
+        pageCount = if (size % itemsPerPage == 0) size / itemsPerPage else size / itemsPerPage + 1
         var recipeBook: Ui? = null
         val craftingTable = paperUi {
             title = Component.text("Crafting Table")
@@ -24,10 +45,10 @@ class LiquipEnhancementsPlugin : JavaPlugin() {
             rows = 5
             fill(0, Slot.RowOneSlotOne, Slot.RowFiveSlotNine, namelessItem(Material.BLACK_STAINED_GLASS_PANE))
             fill(1, Slot.RowTwoSlotTwo, Slot.RowFourSlotFour, namelessItem(Material.AIR))
-            fill(1, Slot.RowTwoSlotSix, Slot.RowFourSlotEight, namelessItem(Material.LIME_STAINED_GLASS_PANE))
+            frame(1, Slot.RowTwoSlotSix, Slot.RowFourSlotEight, namelessItem(Material.LIME_STAINED_GLASS_PANE))
             put(1, Slot.RowThreeSlotNine, namedItem(Material.KNOWLEDGE_BOOK, Component.text("Recipe Book")))
             +listOf(1 to SingleSlotClickPanel(Slot.RowThreeSlotNine.chestSlot) { player, _, _ ->
-                player.closeInventory()
+                recipeBookPages[player.uniqueId] = 0
                 recipeBook?.open(player)
             })
         }
@@ -35,20 +56,40 @@ class LiquipEnhancementsPlugin : JavaPlugin() {
             title = Component.text("Recipe Book")
             type = InventoryType.CHEST
             rows = 6
-            fill(0, Slot.RowOneSlotOne, Slot.RowSixSlotNine, namelessItem(Material.BLACK_STAINED_GLASS_PANE))
-            fill(1, Slot.RowTwoSlotTwo, Slot.RowFiveSlotEight, namelessItem(Material.AIR))
+            frame(0, Slot.RowOneSlotOne, Slot.RowSixSlotNine, namelessItem(Material.BLACK_STAINED_GLASS_PANE))
             put(1, Slot.RowThreeSlotNine, namedItem(Material.CRAFTING_TABLE, Component.text("Crafting Table")))
             put(1, Slot.RowSixSlotFour, namedItem(Material.SPECTRAL_ARROW, Component.text("Previous Page")))
-            put(1, Slot.RowSixSlotFive, namedItem(Material.PAPER, Component.text("Page 0/0")))
+            put(1, Slot.RowSixSlotFive, namedItem(Material.PAPER, Component.text("Page ???/???")))
             put(1, Slot.RowSixSlotSix, namedItem(Material.SPECTRAL_ARROW, Component.text("Next Page")))
             +listOf(1 to SingleSlotClickPanel(Slot.RowThreeSlotNine.chestSlot) { player, _, _ ->
-                player.closeInventory()
+                recipeBookPages.removeInt(player.uniqueId)
                 craftingTable.open(player)
             }, 1 to SingleSlotClickPanel(Slot.RowSixSlotFour.chestSlot) { player, view, _ ->
-                player.sendMessage(Component.text("Previous"))
+                val previousPage = recipeBookPages.getInt(player.uniqueId)
+                val newPage = max(previousPage - 1, 0)
+                if (previousPage != newPage) {
+                    recipeBookPages[player.uniqueId] = newPage
+                    updateRecipeBook(player, view.topInventory)
+                }
             }, 1 to SingleSlotClickPanel(Slot.RowSixSlotSix.chestSlot) { player, view, _ ->
-                player.sendMessage(Component.text("Next"))
+                val previousPage = recipeBookPages.getInt(player.uniqueId)
+                val newPage = min(previousPage + 1, pageCount - 1)
+                if (previousPage != newPage) {
+                    recipeBookPages[player.uniqueId] = newPage
+                    updateRecipeBook(player, view.topInventory)
+                }
+            }, 1 to ClickPanel(Slot.RowTwoSlotTwo.chestSlot, Slot.RowFourSlotEight.chestSlot, 9) { player, _, slot ->
+                val row = Slot.getRow(9, slot) - 1
+                val column = Slot.getColumn(9, slot) - 1
+                val i = row * 7 + column
+                player.sendMessage(Component.text("${catalogue.getOrNull(i)?.first?.key()}"))
             })
+            onOpen(2) { player, inventory -> updateRecipeBook(player, inventory) }
+            onClose { player, _, reason ->
+                if (reason != InventoryCloseEvent.Reason.OPEN_NEW) {
+                    recipeBookPages.removeInt(player.uniqueId)
+                }
+            }
         }
         getCommand("test")?.setExecutor { sender, _, _, _ ->
             if (sender !is Player) {
@@ -61,5 +102,25 @@ class LiquipEnhancementsPlugin : JavaPlugin() {
 
     private fun namelessItem(material: Material) = ItemStack(material).apply { editMeta { it.displayName(Component.empty()) } }
 
-    private fun namedItem(material: Material, name: Component) = ItemStack(material).apply { editMeta { it.displayName(name) } }
+    private fun namedItem(material: Material, name: Component) = ItemStack(material).apply {
+        editMeta { it.displayName(name.decoration(TextDecoration.ITALIC, false)) }
+    }
+
+    private fun updateRecipeBook(player: Player, inventory: Inventory) {
+        for (row in 1..4) {
+            for (column in 1..7) {
+                inventory.setItem(row * 9 + column, null)
+            }
+        }
+        val page = recipeBookPages.getInt(player.uniqueId)
+        inventory.getItem(Slot.RowSixSlotFive.chestSlot)
+            ?.editMeta { it.displayName(Component.text("Page ${page + 1}/$pageCount")) }
+        val startIndex = page * itemsPerPage
+        val endIndex = min(startIndex + itemsPerPage, catalogue.size)
+        for (i in startIndex until endIndex) {
+            val column = Slot.getColumn(7, i) + 1
+            val row = Slot.getRow(7, i) + 1
+            inventory.setItem(row * 9 + column, catalogue[i].second)
+        }
+    }
 }
