57b36690a2a2a611d1bacd5f3248c766f38988dd
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-30 18:12:45 +0100

    Register AspectOfTheEnd feature

--- src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
+++ src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
@@ -1,10 +1,12 @@
 package io.github.liquip.enhancements
 
 import io.github.liquip.api.LiquipProvider
+import io.github.liquip.enhancements.feature.AspectOfTheEnd
 import org.bukkit.plugin.java.JavaPlugin
 
 class LiquipEnhancementsPlugin : JavaPlugin() {
     override fun onEnable() {
         val api = LiquipProvider.get()
+        AspectOfTheEnd().register(api)
     }
 }
