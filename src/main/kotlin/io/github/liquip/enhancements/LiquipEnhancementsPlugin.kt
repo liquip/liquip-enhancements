76a1784fff14261bfa161d31235148987184d10d
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-03 15:57:38 +0100

    Register Jellyfish feature

--- src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
+++ src/main/kotlin/io/github/liquip/enhancements/LiquipEnhancementsPlugin.kt
@@ -2,11 +2,18 @@ package io.github.liquip.enhancements
 
 import io.github.liquip.api.LiquipProvider
 import io.github.liquip.enhancements.feature.AspectOfTheEnd
+import io.github.liquip.enhancements.feature.Jellyfish
 import org.bukkit.plugin.java.JavaPlugin
 
 class LiquipEnhancementsPlugin : JavaPlugin() {
     override fun onLoad() {
         val api = LiquipProvider.get()
         AspectOfTheEnd().register(api)
+        Jellyfish.PurpleJellyfish(this)
+            .register(api)
+        Jellyfish.AquaJellyfish(this)
+            .register(api)
+        Jellyfish.OrangeJellyfish(this)
+            .register(api)
     }
 }
