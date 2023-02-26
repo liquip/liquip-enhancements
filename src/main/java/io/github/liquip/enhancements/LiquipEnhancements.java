4f6ff08aa3204ef4d684c7d6838abe76d7956901
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-26 11:22:21 +0100

    Migrate to new paper plugin system

--- src/main/java/io/github/liquip/enhancements/LiquipEnhancements.java
+++ src/main/java/io/github/liquip/enhancements/LiquipEnhancements.java
@@ -5,7 +5,6 @@ import io.github.liquip.api.LiquipProvider;
 import io.github.liquip.enhancements.event.EventListeners;
 import io.github.liquip.enhancements.item.StaffOfPower;
 import io.github.liquip.enhancements.item.TeleportStaff;
-import io.github.liquip.enhancements.item.armor.Armor;
 import io.github.liquip.enhancements.item.armor.LiquipArmor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,20 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 
 public class LiquipEnhancements extends JavaPlugin {
-    private Liquip api;
-    private List<Armor> armors;
-
-    @Override
-    public void onLoad() {
-        api = LiquipProvider.get();
-        new StaffOfPower().register(api);
-        new TeleportStaff().register(api);
-        armors = List.of(new LiquipArmor(api));
-    }
-
     @Override
     public void onEnable() {
+        final Liquip api = LiquipProvider.get();
+        new StaffOfPower().register(api);
+        new TeleportStaff().register(api);
         Bukkit.getPluginManager()
-            .registerEvents(new EventListeners(api, armors), this);
+            .registerEvents(new EventListeners(api, List.of(new LiquipArmor(api))), this);
     }
 }
