080f79c2390c13f6f7eebd8e6ba609fb26b4f7a4
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-26 21:55:03 +0100

    Start implementing StaffOfPower

--- src/main/java/io/github/liquip/enhancements/LiquipEnhancements.java
+++ src/main/java/io/github/liquip/enhancements/LiquipEnhancements.java
@@ -2,23 +2,30 @@ package io.github.liquip.enhancements;
 
 import io.github.liquip.api.Liquip;
 import io.github.liquip.api.LiquipProvider;
-import io.github.liquip.enhancements.event.EventListeners;
+import io.github.liquip.enhancements.event.ArmorChangeListener;
+import io.github.liquip.enhancements.event.InteractListener;
 import io.github.liquip.enhancements.item.StaffOfPower;
 import io.github.liquip.enhancements.item.TeleportStaff;
 import io.github.liquip.enhancements.item.armor.LiquipArmor;
 import io.github.liquip.enhancements.item.armor.TitaniumArmor;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
+import java.util.Map;
 
 public class LiquipEnhancements extends JavaPlugin {
     @Override
     public void onEnable() {
         final Liquip api = LiquipProvider.get();
-        new StaffOfPower().register(api);
-        new TeleportStaff().register(api);
-        Bukkit.getPluginManager()
-            .registerEvents(new EventListeners(api, List.of(new LiquipArmor(api), new TitaniumArmor(api))), this);
+        final StaffOfPower staffOfPower = new StaffOfPower(api);
+        final TeleportStaff teleportStaff = new TeleportStaff(api);
+        final LiquipArmor liquipArmor = new LiquipArmor(api);
+        final TitaniumArmor titaniumArmor = new TitaniumArmor(api);
+        final PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new ArmorChangeListener(api, List.of(liquipArmor, titaniumArmor)), this);
+        pluginManager.registerEvents(new InteractListener(api,
+            Map.of(StaffOfPower.KEY, staffOfPower::onInteract, TeleportStaff.KEY, teleportStaff::onInteract)), this);
     }
 }
--- src/main/java/io/github/liquip/enhancements/item/StaffOfPower.java
+++ src/main/java/io/github/liquip/enhancements/item/StaffOfPower.java
@@ -1,33 +1,35 @@
 package io.github.liquip.enhancements.item;
 
 import io.github.liquip.api.Liquip;
-import io.github.liquip.api.item.Feature;
-import io.github.liquip.api.item.Item;
+import io.github.liquip.paper.core.item.FixedItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class StaffOfPower implements Feature {
-    private final NamespacedKey key = new NamespacedKey("liquip", "staff_of_power");
+import java.util.HashSet;
+import java.util.Set;
 
-    public void register(@NotNull Liquip api) {
-        api.getFeatureRegistry()
-            .register(key, this);
+public final class StaffOfPower {
+    public static final NamespacedKey KEY = new NamespacedKey("liquip", "staff_of_power");
+    private final Liquip api;
+    private final Set<Void> rays;
+
+    public StaffOfPower(@NotNull Liquip api) {
+        this.api = api;
+        rays = new HashSet<>();
+        api.getItemRegistry()
+            .register(KEY, new FixedItem.Builder().api(api)
+                .key(KEY)
+                .material(Material.GOLDEN_SHOVEL)
+                .name(Component.text("Staff Of Power")
+                    .decoration(TextDecoration.ITALIC, false))
+                .build());
     }
 
-    @Override
-    public @NotNull NamespacedKey getKey() {
-        return key;
-    }
-
-    @Override
-    public void initialize(@NonNull Item item) {
-        // TODO
-    }
-
-    @Override
-    public void apply(@NonNull Item item, @NonNull ItemStack itemStack) {
+    public void onInteract(@NotNull PlayerInteractEvent event) {
         // TODO
     }
 }
