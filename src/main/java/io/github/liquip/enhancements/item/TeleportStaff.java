9c82f51c47d36d365b2f5a9c3519b6efb4d64415
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-26 21:53:58 +0100

    Remove legacy API usage

--- src/main/java/io/github/liquip/enhancements/item/TeleportStaff.java
+++ src/main/java/io/github/liquip/enhancements/item/TeleportStaff.java
@@ -1,8 +1,6 @@
 package io.github.liquip.enhancements.item;
 
 import io.github.liquip.api.Liquip;
-import io.github.liquip.api.item.Feature;
-import io.github.liquip.api.item.Item;
 import io.github.liquip.paper.core.item.FixedItem;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
@@ -11,36 +9,23 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
 
-public class TeleportStaff implements Feature {
-    private final NamespacedKey key = new NamespacedKey("liquip", "teleport_staff");
-    private final Vector yOne = new Vector(0, 1, 0);
+public record TeleportStaff(@NotNull Liquip api) {
+    public static final NamespacedKey KEY = new NamespacedKey("liquip", "teleport_staff");
+    private static final Vector Y_ONE = new Vector(0, 1, 0);
 
-    public void register(@NotNull Liquip api) {
+    public TeleportStaff {
         api.getItemRegistry()
-            .register(key, new FixedItem.Builder().api(api)
-                .key(key)
+            .register(KEY, new FixedItem.Builder().api(api)
+                .key(KEY)
                 .material(Material.STICK)
                 .name(Component.text("Teleport Staff"))
-                .feature(this)
                 .build());
     }
 
-    @Override
-    public @NotNull NamespacedKey getKey() {
-        return key;
-    }
-
-    @Override
-    public void initialize(@NonNull Item item) {
-        item.registerEvent(PlayerInteractEvent.class, this::onInteract);
-    }
-
-    private void onInteract(PlayerInteractEvent event, ItemStack stack) {
+    public void onInteract(@NotNull PlayerInteractEvent event) {
         final Player player = event.getPlayer();
         if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
@@ -48,6 +33,6 @@ public class TeleportStaff implements Feature {
         final Location eyeLocation = player.getEyeLocation();
         player.teleport(eyeLocation.add(eyeLocation.getDirection()
             .multiply(8)
-            .subtract(yOne)));
+            .subtract(Y_ONE)));
     }
 }
